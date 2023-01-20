346990241918e992fb9e7b120cf6e69199b72f6c
kj1010g <kjkagit@gmail.com>
2023-01-20 16:24:48 +0530

    added a few assertions which make sure that X-principal header value has correct format and valid data

--- core/apis/decorators.py
+++ core/apis/decorators.py
@@ -2,7 +2,9 @@ import json
 from flask import request
 from core.libs import assertions
 from functools import wraps
-
+from core.models.students import Student
+from core.models.teachers import Teacher
+from json.decoder import JSONDecodeError
 
 class Principal:
     def __init__(self, user_id, student_id=None, teacher_id=None):
@@ -24,17 +26,30 @@ def auth_principal(func):
     def wrapper(*args, **kwargs):
         p_str = request.headers.get('X-Principal')
         assertions.assert_auth(p_str is not None, 'principal not found')
-        p_dict = json.loads(p_str)
+        try:
+            p_dict = json.loads(p_str)
+        except JSONDecodeError:
+            assertions.assert_valid(False, 'Invalid X-Principal header value')
         p = Principal(
-            user_id=p_dict['user_id'],
+            user_id=p_dict.get('user_id'),
             student_id=p_dict.get('student_id'),
             teacher_id=p_dict.get('teacher_id')
         )
+        assertions.assert_auth(p.user_id is not None, 'user_id not found in principal')
+        assertions.assert_valid(type(p.user_id)==int, 'user_id in header X-Principal should be an integer')
 
         if request.path.startswith('/student'):
             assertions.assert_true(p.student_id is not None, 'requester should be a student')
+            assertions.assert_valid(type(p.student_id)==int, 'student_id in header X-Principal should be an integer')
+            student = Student.get_by_id(p.student_id)
+            assertions.assert_found(student, 'No student with this id was found')
+            assertions.assert_auth(student.user_id == p.user_id, 'Wrong user_id for given student_id')
         elif request.path.startswith('/teacher'):
             assertions.assert_true(p.teacher_id is not None, 'requester should be a teacher')
+            assertions.assert_valid(type(p.teacher_id)==int, 'teacher_id in header X-Principal should be an integer')
+            teacher = Teacher.get_by_id(p.teacher_id)
+            assertions.assert_found(teacher, 'No teacher with this id was found')
+            assertions.assert_auth(teacher.user_id == p.user_id, 'Wrong user_id for given teacher_id')
         else:
             assertions.assert_found(None, 'No such api')
 
